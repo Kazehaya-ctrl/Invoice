0ef90b737bb87ffab8737d132d2a57d8f3273a44
Kazehaya-ctrl <mahitomaki96@gmail.com>
2024-12-26 16:15:40 +0530

    upt: unknown

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 			alert("Select a file");
 			return;
 		}
-
+		console.log(selectFile.type);
 		const reader = new FileReader();
 		reader.onloadend = async () => {
 			const base64String = reader.result as string;
